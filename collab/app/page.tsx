4b4ac3e3c1995f1524376d52763b6b34814aaa24
Akashh001 <protongithub1@protonmail.com>
2024-03-23 10:03:06 +0530

    Setting up File Structure and shadcn

--- collab/app/page.tsx
+++ collab/app/page.tsx
@@ -1,10 +1,7 @@
-import { Room } from "./Room";
-import { CollaborativeApp } from "./CollaborativeApp";
-
 export default function Page() {
   return (
-    <Room>
-      <CollaborativeApp />
-    </Room>
+    <div className="w-full h-screen flex justify-center items-center text-center">
+      <h1 className="text-2xl text-white">Hello figma clone!</h1>
+    </div>
   );
 }
