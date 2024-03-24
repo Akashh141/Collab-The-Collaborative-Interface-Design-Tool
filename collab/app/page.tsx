a20dc3bf8c6ae7e95f7aa38bc73ad3eee8e9fc33
Akashh001 <protongithub1@protonmail.com>
2024-03-24 10:15:25 +0530

    Adding cursor functionality

--- collab/app/page.tsx
+++ collab/app/page.tsx
@@ -1,7 +1,6 @@
-export default function Page() {
-  return (
-    <div className="w-full h-screen flex justify-center items-center text-center">
-      <h1 className="text-2xl text-white">Hello figma clone!</h1>
-    </div>
-  );
-}
+import dynamic from "next/dynamic";
+
+  
+const App = dynamic(() => import("./App"), { ssr: false });
+
+export default App;
